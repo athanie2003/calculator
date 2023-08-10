17d70c614fe583a9aaaad09389936f744435e5cc
Aaravan <aaravan2003@gmail.com>
2023-08-10 11:52:25 -0400

    Create opreate function that calls math functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const num1 = prompt();
-const op = prompt();
-const num2 = prompt();
+const num1 = prompt(`num1: `);
+const op = prompt(`operator: `);
+const num2 = prompt(`num2: `);
+
+console.log(operate(num1, op, num2));
 
 
 function add(num1, num2){
@@ -16,7 +18,25 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-console.log(add(3,5));
-console.log(subtract(3,5));
-console.log(multiply(3,5));
-console.log(divide(3,5));
+function operate(num1, op, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    const operations = {
+        "+": add(num1, num2),
+        "-": subtract(num1, num2),
+        "*": multiply(num1, num2),
+        "/": divide(num1, num2)
+    };
+
+    if(!operations[op] || isNaN(num1) || isNaN(num2)){
+        return NaN;
+    }
+
+    return operations[op];
+}
+
+// console.log(add(3,5));
+// console.log(subtract(3,5));
+// console.log(multiply(3,5));
+// console.log(divide(3,5));
